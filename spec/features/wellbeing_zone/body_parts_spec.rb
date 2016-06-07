d210fe99ac58385ded60da83477ac418d1188c0f
James Doyley <jdoyley@gmail.com>
2016-06-07 14:34:41 +0100

    add body part summary to view

--- spec/features/wellbeing_zone/body_parts_spec.rb
+++ spec/features/wellbeing_zone/body_parts_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 feature "visiting wellbeing zone body parts" do
   scenario "clicking through from the main wellbeing zone" do
-    create(:body_part, name: "Ankle")
+    create(:body_part, name: "Ankle", summary: "Some summary")
 
     visit wellbeing_zone_root_path
     click_link "Ankle"
 
     expect(page).to have_css "h1", text: "Ankle"
+    expect(page).to have_css ".body-part-summary", text: "Some summary"
   end
 end
