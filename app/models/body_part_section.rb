b18add05644dd91ea50b02da56370cce6db5382e
James Doyley <jdoyley@gmail.com>
2016-06-08 14:41:22 +0100

    add body part section validations

--- app/models/body_part_section.rb
+++ app/models/body_part_section.rb
@@ -2,5 +2,9 @@ class BodyPartSection < ActiveRecord::Base
   mount_uploader :image, BodyPartSectionUploader
 
   belongs_to :body_part, counter_cache: true
+
+  validates :body_part, presence: true
+  validates :content, presence: true
+  validates :title, presence: true
   
 end
--- spec/models/body_part_section_spec.rb
+++ spec/models/body_part_section_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe BodyPartSection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:body_part) }
+    it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:title) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:body_part).counter_cache }
+  end
 end
