5651e7bfb2054e353ea0a87cbabf6f659a7f76dd
James Doyley <jdoyley@gmail.com>
2016-06-08 16:39:14 +0100

    amend body part section admin controller test

--- spec/controllers/optimadmin/body_part_section_controller_spec.rb
+++ spec/controllers/optimadmin/body_part_section_controller_spec.rb
@@ -9,35 +9,27 @@ describe Optimadmin::BodyPartSectionsController, type: :controller, body_part_se
   describe '#create' do
     context 'when Body part section is valid' do
       it 'redirects to index on normal save' do
-        body_part_section = stub_valid_body_part_section
+        body_part = create(:body_part)
+        body_part_section = stub_valid_body_part_section(body_part: body_part)
 
-        process :create, method: :post, params: {
+        post :create, {
+          body_part_id: body_part_section.body_part_id,
           body_part_section: body_part_section.attributes,
           commit: 'Save'
         }
 
-        expect(response).to redirect_to(body_part_sections_path)
-        expect(flash[:notice]).to eq('Body part section was successfully created.')
-      end
-
-      it 'redirects to edit on save and continue editing' do
-        body_part_section = stub_valid_body_part_section
-
-        process :create, method: :post, params: {
-          body_part_section: body_part_section.attributes,
-          commit: 'Save and continue editing'
-        }
-
-        expect(response).to redirect_to(edit_body_part_section_path(body_part_section))
+        expect(response).to redirect_to(body_part_body_part_sections_path(body_part))
         expect(flash[:notice]).to eq('Body part section was successfully created.')
       end
     end
 
     context 'when Body part section is invalid' do
       it 'does not redirect' do
-        body_part_section = stub_invalid_body_part_section
+        body_part = create(:body_part)
+        body_part_section = stub_invalid_body_part_section(body_part: body_part)
 
-        process :create, method: :post, params: {
+        post :create, {
+          body_part_id: body_part,
           body_part_section: body_part_section.attributes,
           commit: 'Save'
         }
@@ -50,37 +42,26 @@ describe Optimadmin::BodyPartSectionsController, type: :controller, body_part_se
   describe '#update' do
     context 'when Body part section is valid' do
       it 'redirects to index on normal save' do
-        body_part_section = stub_valid_body_part_section
+        body_part = create(:body_part)
+        body_part_section = stub_valid_body_part_section(body_part: body_part)
 
-        process :update, method: :patch, params: {
+        patch :update, {
           id: body_part_section.id,
           body_part_section: body_part_section.attributes,
           commit: 'Save'
         }
 
-        expect(response).to redirect_to(body_part_sections_path)
-        expect(flash[:notice]).to eq('Body part section was successfully updated.')
-      end
-
-      it 'redirects to edit on save and continue editing' do
-        body_part_section = stub_valid_body_part_section
-
-        process :update, method: :patch, params: {
-          id: body_part_section.id,
-          body_part_section: body_part_section.attributes,
-          commit: 'Save and continue editing'
-        }
-
-        expect(response).to redirect_to(edit_body_part_section_path(body_part_section))
+        expect(response).to redirect_to(body_part_body_part_sections_path(body_part.id))
         expect(flash[:notice]).to eq('Body part section was successfully updated.')
       end
     end
 
     context 'when Body part section is invalid' do
       it 'does not redirect' do
-        body_part_section = stub_invalid_body_part_section
+        body_part = create(:body_part)
+        body_part_section = stub_invalid_body_part_section(body_part: body_part)
 
-        process :update, method: :patch, params: {
+        patch :update, {
           id: body_part_section.id,
           body_part_section: body_part_section.attributes,
           commit: 'Save'
@@ -91,20 +72,21 @@ describe Optimadmin::BodyPartSectionsController, type: :controller, body_part_se
     end
   end
 
-  def stub_valid_body_part_section
-    body_part_section = build_stubbed(:body_part_section)
+
+  def stub_valid_body_part_section(body_part: nil)
+    body_part_section = build_stubbed(:body_part_section, body_part: body_part)
     allow(BodyPartSection).to receive(:new).and_return(body_part_section)
     allow(body_part_section).to receive(:save).and_return(true)
-    allow(BodyPartSection).to receive_message_chain(:friendly, :find).and_return(body_part_section)
+    allow(BodyPartSection).to receive(:find).and_return(body_part_section)
     allow(body_part_section).to receive(:update).and_return(true)
     body_part_section
   end
 
-  def stub_invalid_body_part_section
-    body_part_section = build_stubbed(:body_part_section)
+  def stub_invalid_body_part_section(body_part: nil)
+    body_part_section = build_stubbed(:body_part_section, body_part: body_part)
     allow(BodyPartSection).to receive(:new).and_return(body_part_section)
     allow(body_part_section).to receive(:save).and_return(false)
-    allow(BodyPartSection).to receive_message_chain(:friendly, :find).and_return(body_part_section)
+    allow(BodyPartSection).to receive(:find).and_return(body_part_section)
     allow(body_part_section).to receive(:update).and_return(false)
     body_part_section
   end
