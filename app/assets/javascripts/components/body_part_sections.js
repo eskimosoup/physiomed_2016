9dca5dbcdf171d9d643643382e5dcb44c9e3c183
James Doyley <jdoyley@gmail.com>
2016-06-10 10:20:07 +0100

    Add basic styling to body part sections

--- app/assets/javascripts/components/body_part_sections.js
+++ app/assets/javascripts/components/body_part_sections.js
@@ -1,28 +1,25 @@
 var tabLinks = new Array();
 var bodyPartSections = new Array();
 
-$('.body-part-sections ul#tabs li a').click(function(e) {
+$('.body-part-sections-container ul#tabs li a').click(function(e) {
   var linkClicked = $(this).attr("href");
   switchTab(linkClicked);
   e.preventDefault();
 });
 
 function switchTab(linkClicked) {
-  $('.body-part-sections ul#tabs li').removeClass('current');
+  console.log(linkClicked);
+  $('.body-part-sections-container ul#tabs li').removeClass('current');
   $('.body-part-section').removeClass('current');
-  console.log($.inArray(linkClicked, tabLinks));
   tabLinks[linkClicked].addClass('current');
   bodyPartSections[linkClicked].addClass('current');
 }
 
 $(document).ready(function() {
-  var tabs = $('.body-part-sections ul#tabs li');
+  var tabs = $('.body-part-sections-container ul#tabs li');
   tabs.each(function() {
     var id = $(this).find("a").attr("href")
     tabLinks[id] = $(this);
     bodyPartSections[id] = $(id);
   });
-  //console.log(bodyPartSections);
-//  console.log(firstTab.find("a").attr("href"));
-  //console.log(tabLinks);
 });
