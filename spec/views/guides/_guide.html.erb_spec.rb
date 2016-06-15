876daa313c026a84e9259c69128d944759db0423
James Doyley <jdoyley@gmail.com>
2016-06-15 16:12:07 +0100

    Add guide view partial

---
+++ spec/views/guides/_guide.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'guides/_guide.html.erb' do
+  context 'has an image' do
+    it 'renders an image' do
+      guide = create(:guide_with_image)
+
+      render_guide(guide)
+
+      expect(rendered).to have_css "img"
+    end
+  end
+
+  context 'has no image' do
+    it 'does not render an image' do
+      guide = create(:guide, image: nil)
+
+      render_guide(guide)
+
+      expect(rendered).not_to have_css "img"
+    end
+  end
+
+  def render_guide(guide)
+    render "guides/guide", guide: guide
+  end
+end
