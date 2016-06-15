6112fa6e79a391eeb3e6407ca354e2d0c3e07ebd
James Doyley <jdoyley@gmail.com>
2016-06-15 14:20:39 +0100

    Add wellbeing zone testimonial section spec

---
+++ spec/views/wellbeing_zone/shared/_testimonials_section.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "wellbeing_zone/shared/_testimonials_section.html.erb" do
+  context 'with testimonials' do
+    it 'renders the testimonals section' do
+      testimonials = build_stubbed_list(:testimonial, 2)
+
+      render "wellbeing_zone/shared/testimonials_section", testimonials: testimonials
+
+      expect(rendered).to have_css ".testimonials-container"
+    end
+  end
+
+  context 'without testimonials' do
+    it 'renders the testimonals section' do
+      render "wellbeing_zone/shared/testimonials_section", testimonials: []
+
+      expect(rendered).to be_blank
+    end
+  end
+end
