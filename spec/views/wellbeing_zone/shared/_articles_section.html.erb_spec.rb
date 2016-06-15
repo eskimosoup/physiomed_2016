85f085a805286c024952b11a548fbbd6ff59a8f8
James Doyley <jdoyley@gmail.com>
2016-06-15 14:02:22 +0100

    Add wellbeing_zone/shared/_articles_container.html.erb spec

---
+++ spec/views/wellbeing_zone/shared/_articles_section.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'wellbeing_zone/shared/_articles_section.html.erb' do
+  context 'with articles' do
+    it 'renders the articles container' do
+      articles = build_stubbed_list(:article, 2)
+
+      render "wellbeing_zone/shared/articles_section", articles: articles
+
+      expect(rendered).to have_css ".articles-container"
+    end
+  end
+
+  it 'renders nothing' do
+    render "wellbeing_zone/shared/articles_section", articles: []
+
+    expect(rendered).to be_blank
+  end
+end
