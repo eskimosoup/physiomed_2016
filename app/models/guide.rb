da9ec73b7230da58bcf6bbf16c66941d71b83c91
James Doyley <jdoyley@gmail.com>
2016-06-15 15:39:02 +0100

    Add model validations to guides

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,4 +1,7 @@
 class Guide < ActiveRecord::Base
   mount_uploader :image, GuideUploader
   mount_uploader :file, Optimadmin::DocumentUploader
+
+  validates :title, presence: true
+  validates :file, presence: true
 end
