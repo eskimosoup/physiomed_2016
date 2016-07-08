9092eab427604f079e676038773560c2a5b850cf
Paul <pauloptimised@users.noreply.github.com>
2016-07-08 16:07:01 +0100

    Update limiting

--- app/models/what_we_do_facade.rb
+++ app/models/what_we_do_facade.rb
@@ -1,6 +1,6 @@
 class WhatWeDoFacade
   def what_we_dos
     @what_we_dos ||= WhatWeDo.eager_load(:displayed_links)
-                             .displayed.order(position: :asc).merge(WhatWeDoLink.order(position: :asc))
+                             .displayed.order(position: :asc).limit(3).merge(WhatWeDoLink.order(position: :asc))
   end
 end
