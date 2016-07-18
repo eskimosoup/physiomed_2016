8632262ec9185540884ae6d11ce9d8235c1062f9
Paul <pauloptimised@users.noreply.github.com>
2016-07-18 14:50:11 +0100

    Guides

--- app/uploaders/guide_uploader.rb
+++ app/uploaders/guide_uploader.rb
@@ -2,4 +2,8 @@ class GuideUploader < Optimadmin::ImageUploader
   version :index do
     process resize_to_fill: [220, 185]
   end
+
+  version :show do
+    process resize_to_fill: [165, 216]
+  end
 end
