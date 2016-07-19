851d8eab9c1de23cccef87daf857fd5cc4c64402
Paul <pauloptimised@users.noreply.github.com>
2016-07-19 13:27:58 +0100

    Articles

--- app/models/general_wellbeing_facade.rb
+++ app/models/general_wellbeing_facade.rb
@@ -1,9 +1,12 @@
 class GeneralWellbeingFacade < WellbeingZoneFacade
-
   delegate :content, to: :category, allow_nil: true
 
   def guides
     @guides ||= Guide.for_category(category).displayed
+  end
+
+  def articles
+    @articles ||= Article.for_category(category).displayed
   end
 
   def body_parts
