f5df648a95f4a8cc664694591dc5e41d6d46fb80
Paul <pauloptimised@users.noreply.github.com>
2016-07-21 09:20:01 +0100

    GA event amend

--- app/assets/javascripts/components/google-analytics-events.js
+++ app/assets/javascripts/components/google-analytics-events.js
@@ -1,6 +1,10 @@
-$(document).on('click', '.ga-event', function() {
-	var eventName = $(this).attr('data-ga-event-name');
-	var itemName  = $(this).attr('data-ga-item-name');
+$(function() {
+  $('.ga-event').attr('target', '_blank');
+});
+
+$(document).on('click', '.ga-event', function(e) {
+	var eventName = $(this).data('event-name');
+	var itemName  = $(this).data('item-name');
 
 	if ( eventName && itemName ) {
 		ga('send', 'event', eventName, 'click', eventName + ': ' + itemName, 1);
