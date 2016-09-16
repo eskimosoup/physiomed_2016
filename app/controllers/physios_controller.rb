f88228c009d55c3a7cebcf37caae00c2aa3dfadb
optimised <noreply@optimised.today>
2016-09-16 15:08:46 +0100

    Add Pronto logo and amend footer accreditations

--- app/controllers/physios_controller.rb
+++ app/controllers/physios_controller.rb
@@ -1,7 +1,7 @@
 class PhysiosController < ApplicationController
-  http_basic_authenticate_with(
-    name: ENV['PHYSIO_USERNAME'], password: ENV['PHYSIO_PASSWORD']
-  )
+  # http_basic_authenticate_with(
+  #  name: ENV['PHYSIO_USERNAME'], password: ENV['PHYSIO_PASSWORD']
+  # )
 
   def index
   end
