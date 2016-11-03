4ad31dd7bdfa3a4090f8625de74ba9eb7074f179
optimised <noreply@optimised.today>
2016-11-03 14:26:34 +0000

    See all guides views

--- app/controllers/wellbeing_zone/body_parts_controller.rb
+++ app/controllers/wellbeing_zone/body_parts_controller.rb
@@ -1,8 +1,8 @@
 module WellbeingZone
   class BodyPartsController < ApplicationController
     def show
-      body_part = BodyPart.displayed.friendly.find(params[:id])
-      @body_part_facade = BodyPartFacade.new(body_part)
+      @body_part = BodyPart.displayed.friendly.find(params[:id])
+      @body_part_facade = BodyPartFacade.new(@body_part)
     end
   end
 end
