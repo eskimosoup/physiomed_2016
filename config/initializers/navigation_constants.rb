e22cbe854f936598caafcbe1a5d7e297427aaed1
optimised <noreply@optimised.today>
2017-11-15 09:51:39 +0000

    Dropdown

--- config/initializers/navigation_constants.rb
+++ config/initializers/navigation_constants.rb
@@ -3,5 +3,6 @@ NavigationMenus = %w( header footer client_zone_header ).freeze
 NavigationLinks = {
   'Module Page' => 'Optimadmin::ModulePage',
   'Dynamic Page' => 'Page',
-  'External Link' => 'Optimadmin::ExternalLink'
+  'External Link' => 'Optimadmin::ExternalLink',
+  'Landing Page' => 'LandingPage',
 }.freeze
