8c962c1dc2fed47234c5bd6d7ff64961e8e86174
optimised <noreply@optimised.today>
2018-01-18 16:22:42 +0000

    Fix list

--- app/controllers/optimadmin/mailchimp/subscribers_controller.rb
+++ app/controllers/optimadmin/mailchimp/subscribers_controller.rb
@@ -2,8 +2,8 @@ module Optimadmin
   module Mailchimp
     class SubscribersController < BaseController
       def show
-        @member = gibbon.lists(cquin_list).members(params[:email_id]).retrieve
-        @activity = gibbon.lists(cquin_list).members(params[:email_id]).activity.retrieve
+        # @member = gibbon.lists(cquin_list).members(params[:email_id]).retrieve
+        # @activity = gibbon.lists(cquin_list).members(params[:email_id]).activity.retrieve
 
         respond_to do |format|
           format.html
