1cafe0560286a608856ed981a5855a857228e79a
Paul <pauloptimised@users.noreply.github.com>
2018-05-24 16:02:40 +0100

    Prefer YouTube nocookie

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,5 +1,5 @@
 module VideosHelper
   def iframe_for(video, width: 300, height: 200)
-    "<iframe width=\"#{width}\" height=\"#{height}\" src=\"//www.youtube.com/embed/#{video.youtube_identifier}?showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>"
+    "<iframe width=\"#{width}\" height=\"#{height}\" src=\"//www.youtube-nocookie.com/embed/#{video.youtube_identifier}?showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>"
   end
 end
