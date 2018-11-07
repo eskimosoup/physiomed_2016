ae0f70f24f3af7fd0bf3a4be47c999100f279b11
Paul <pauloptimised@users.noreply.github.com>
2018-11-07 13:43:01 +0000

    Basic home page

--- app/models/home_facade.rb
+++ app/models/home_facade.rb
@@ -1,14 +1,51 @@
 class HomeFacade
+  def banners
+    @banners ||= Banner.displayed.ordered_by_position
+  end
+
+  def affiliates
+    @affiliates ||= service.sections.displayed.find_by(style: 'affiliates')
+  end
+
+  def clients
+    @clients ||= service.sections.displayed.find_by(style: 'clients')
+  end
+
+  def testimonials
+    @testimonials ||= service.sections.displayed.find_by(style: 'testimonials')
+  end
+
+  def service
+    @service ||= services.first
+  end
+
+  def affiliates?
+    affiliates.present?
+  end
+
+  def testimonials?
+    testimonials.present?
+  end
+
+  def clients?
+    clients.present?
+  end
+
+  def service_standards
+    @service_standards ||= ServiceStandard.displayed.ordered_by_position.where.not(id: [LandingPages::ServiceStandard.pluck(:service_standard_id)])
+  end
+
+  def services
+    @services ||= Service.displayed
+  end
+
+  # --- OLD
   def additional_contents
     @additional_contents ||= AdditionalHomeContent.includes(:video).displayed.ordered_by_position
   end
 
   def articles
     @articles ||= Article.includes(:author).displayed.home.order(date: :desc).limit(10)
-  end
-
-  def banners
-    @banners ||= Banner.displayed.ordered_by_position
   end
 
   def employee_quick_links
@@ -27,20 +64,8 @@ class HomeFacade
     @quick_links ||= QuickLink.displayed.ordered_by_position
   end
 
-  def service_standards
-    @service_standards ||= ServiceStandard.displayed.ordered_by_position.where.not(id: [LandingPages::ServiceStandard.pluck(:service_standard_id)])
-  end
-
   def case_studies
     @case_studies ||= CaseStudy.order(date: :desc).displayed.for_home.limit(10)
-  end
-
-  def testimonials
-    @testimonials ||= Testimonial.displayed.order('RANDOM()').limit(10)
-  end
-
-  def clients
-    @cients ||= Client.displayed.ordered_by_position
   end
 
   def job_listings
