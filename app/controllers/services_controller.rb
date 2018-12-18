7ba3fc0bcea81d91a34d18fa999d5bb149e18d6e
Paul <pauloptimised@users.noreply.github.com>
2018-12-18 16:59:48 +0000

    Amend navigation

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,7 +2,7 @@ class ServicesController < ApplicationController
   def show
     service = Service.friendly.displayed.find(params[:id])
     return redirect_to service, status: :moved_permanently if request.path != service_path(service)
-    @facade = ServiceFacade.new(service)
+    @facade = ServiceFacade.new(service, ::Services::Category.displayed.roots.first)
     render layout: @facade.service_layout
   end
 end
