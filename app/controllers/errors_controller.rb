5e634b2a106842ecd9a27ae2bfd6cbe91d4b8386
pauloptimised <pauloptimised@users.noreply.github.com>
2020-06-02 09:07:19 +0100

    Fix error reporting

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,12 @@ class ErrorsController < ApplicationController
   before_action :status_code
 
   def show
-    render status_code.to_s, status: status_code
+    respond_to do |format|
+      format.html do
+        render status_code.to_s, status: status_code
+      end
+      format.all { head status_code }
+    end
   end
 
   protected
