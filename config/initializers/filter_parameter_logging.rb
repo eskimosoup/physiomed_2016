0a38697ee9feee1e70e79a8be3e6cc8e5e5f3cf2
pauloptimised <pauloptimised@users.noreply.github.com>
2021-02-08 11:53:34 +0000

    Fix admin

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,6 +4,15 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[
+  passw
+  secret
+  token
+  _key
+  crypt
+  salt
+  certificate
+  otp
+  ssn
   password
   password_confirmation
   forename
