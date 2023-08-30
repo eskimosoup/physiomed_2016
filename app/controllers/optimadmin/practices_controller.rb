05fdbbe93e33867f893740d72e100a8bd7f334a8
pauloptimised <pauloptimised@users.noreply.github.com>
2023-08-30 15:55:56 +0100

    Export practices

--- app/controllers/optimadmin/practices_controller.rb
+++ app/controllers/optimadmin/practices_controller.rb
@@ -1,28 +1,39 @@
 module Optimadmin
   class PracticesController < Optimadmin::ApplicationController
-    before_action :set_practice, only: [:show, :edit, :update, :destroy]
+    before_action :set_practice, only: %i[show edit update destroy]
 
     def index
       @practices = Practice.order_by(params[:order])
-        .field_search(params[:search]).page(params[:page]).per(params[:per_page] || 15)
+                           .field_search(params[:search]).page(params[:page]).per(params[:per_page] || 15)
+
+      respond_to do |format|
+        format.html
+        format.csv do
+          csv = ::CsvExports::BaseService.new(::Practice.all).to_csv
+          send_data(
+            csv,
+            type: 'text/csv; charset=iso-8859-1; header=present',
+            disposition: 'attachment',
+            filename: 'practices.csv'
+          )
+        end
+      end
     end
 
-    def show
-    end
+    def show; end
 
     def new
       @practice = Practice.new
     end
 
-    def edit
-    end
+    def edit; end
 
     def create
       @practice = Practice.new(practice_params)
       if @practice.save
         redirect_to_index_or_continue_editing(@practice)
       else
-        render :new
+        render(:new)
       end
     end
 
@@ -30,13 +41,13 @@ module Optimadmin
       if @practice.update(practice_params)
         redirect_to_index_or_continue_editing(@practice)
       else
-        render :edit
+        render(:edit)
       end
     end
 
     def destroy
       @practice.destroy
-      redirect_to practices_url, notice: 'Practice was successfully destroyed.'
+      redirect_to(practices_url, notice: 'Practice was successfully destroyed.')
     end
 
     private
